d20e36c9a8d686e5e16227029f28c4992e35ddf4
Joseph Senders <jasenders@gmail.com>
2018-08-27 23:13:50 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,13 @@
-class Artist
-  attr_accessor :name, :songs
+class Artist 
+  attr_accessor :name 
+
+  @@all = []
 
   def initialize(name)
-    @name = name
-    @songs = []
-  end
+    @name = name 
+    @@all = []
+  end 
 
-  def add_song(song)
-    @songs<<song
-    song.artist = self
-  end
-
-  def songs
-    @songs
-  end
-
-  def genres
-    @songs.collect {|song| song.genre}
-  end
-
-end
+  def self.all 
+  end 
+  
